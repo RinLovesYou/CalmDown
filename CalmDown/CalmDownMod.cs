b43167c439f5b9a520a128e7be4c9b977654fd34
Sarah <xxklintan123xx@gmail.com>
2022-05-12 06:21:12 +0200

    You can not toggle it at runtime apparently

--- CalmDown/CalmDownMod.cs
+++ CalmDown/CalmDownMod.cs
@@ -14,8 +14,7 @@ namespace CalmDown
     public class CalmDownMod : MelonMod
     {
         private static MelonPreferences_Category CalmDownCat = MelonPreferences.CreateCategory("CalmDown");
-        private static MelonPreferences_Entry<bool> Enabled = CalmDownCat.CreateEntry("Enable PanicButton suppression", true);
-        private static VRCInput OriginalInput;
+        private static MelonPreferences_Entry<bool> Enabled = CalmDownCat.CreateEntry("Enable PanicButton suppression (Requires Restart)", true);
         public override void OnApplicationStart()
         {
             MelonCoroutines.Start(WaitForUiManagerInit());
@@ -26,27 +25,11 @@ namespace CalmDown
             while (VRCUiManager.field_Private_Static_VRCUiManager_0 == null) yield return null;
             while (UIManager.field_Private_Static_UIManager_0 == null) yield return null;
 
-            OriginalInput = VRCInputManager.field_Private_Static_Dictionary_2_String_VRCInput_0["PanicButton"];
-
             if(Enabled.Value)
             {
                 VRCInputManager.field_Private_Static_Dictionary_2_String_VRCInput_0.Remove("PanicButton");
                 LoggerInstance.Msg("Panic Button Disabled");
             }
-
-            Enabled.OnValueChanged += (prev, val) =>
-            {
-                if (val)
-                {
-                    VRCInputManager.field_Private_Static_Dictionary_2_String_VRCInput_0.Remove("PanicButton");
-                    LoggerInstance.Msg("Panic Button Disabled");
-                }
-                else
-                {
-                    VRCInputManager.field_Private_Static_Dictionary_2_String_VRCInput_0.Add("PanicButton", OriginalInput);
-                    LoggerInstance.Msg("Panic Button Enabled");
-                }
-            };
         }
     }
 }
